182da218ee125257ce4cd46a6265ccb826fd1274
pl0x0 <alexis.gonzalez13@utp.ac.pa>
2024-09-02 19:49:43 -0500

    Que hay que hacer

--- Taller03/Main.java
+++ Taller03/Main.java
@@ -1,15 +1,6 @@
 class Main {
     public static void main(String[] args) {
-        /*  Utilizar las sentencias, imprimir y leer en java  el if, definir las variables flotantes y enteras. 
-        Definir nombre a las variables del entorno, A,B,C. 
-        Verificar la formula cuadrática (su forma) 
-        Verificar si las raíces pueden ser negativas (formula general de la cuadrática) 
-        Calcular los valores x1, x2 
-        Entrada de datos A, B, C. 
-        Imprimir resultados. 
-       Subir trabajo a la plataforma de la UTP.  
-       Crear objeto lectura de datos, método calcular valores x1, x2,  
-       Crear el método de impresión de valores x1, x2 */
+
        System.out.println("Chucha madre que pereza");
         System.out.println("pene");
     }
