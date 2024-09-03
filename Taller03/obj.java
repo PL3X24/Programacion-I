55c422d4b8825f161a49c120a3974cea2297c95b
Alexis Gonzalez <alexis.gonzalez13@utp.ac.pa>
2024-09-03 07:32:44 -0500

    Incluir la presentacion y modif pequeñas

--- Taller03/obj.java
+++ Taller03/obj.java
@@ -1,12 +1,20 @@
 import java.util.Scanner;
 public class obj {
-    //Metodos que preguntaran y calcularan
+    //Declaracion de variables
     
     private float x1;
     private float x2;
     private float resultado;
 
+    //Metodo de entrada
     public obj() {
+
+        // Salida de presentacion
+        System.out.println(" Universidad tecnologica de panama  ");
+	    System.out.println(" Estudiante Oliver Batista 8-1028-1845 ");
+        System.out.println(" Estudiante Alexis Gonzales 8-1026-1272 ");
+        
+        //Entrada del usuario
         Scanner scanner = new Scanner(System.in);
         System.out.println("Programa para calcular la formula general");
         System.out.println("Ingrese la variable A:");
@@ -17,9 +25,10 @@ public class obj {
         int C = scanner.nextInt();
         Procedimiento(A, B, C);
     }
+        //Metodo que resuelve la operacion
         private void Procedimiento ( int A, int B, int C){
         
-        //discriminante
+        //Calcular discriminante antes de proceder
         float discr = B * B -4 * A * C;
         resultado = discr;
             if (discr < 0){
@@ -31,6 +40,8 @@ public class obj {
                 x2 = (-B - (float) Math.sqrt(discr)) / (2 * A);
         }
     }
+
+    //Metodos para devolver la respuesta en main
     public float getX1() {
         return x1;
     }
