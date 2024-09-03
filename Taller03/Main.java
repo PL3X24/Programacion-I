82ca8319680ca1e884065333cc6e9239f8bca4bb
Alexis Gonzalez <alexis.gonzalez13@utp.ac.pa>
2024-09-02 21:44:11 -0500

    Creacion de metodo

--- Taller03/Main.java
+++ Taller03/Main.java
@@ -2,20 +2,10 @@
 El profe quiere usar objetos y metodos nada directo
  */
 
-import java.util.Scanner;
-
 class Main {
     public static void main(String[] args) {
         
-        //Creacion del scanner
-        Scanner scanner = new Scanner(System.in);
-
-        //Entrada del usuario
-        /* System.out.println("Programa para calcular la formula general");
-        System.out.println("Ingrese la variable A:");
-        int A = scanner.nextInt(); */
-        
-
+        obj.Usuario();
         /* Chistoso JIjijija (Porfavor borrar ahorita ta de motivacion)
         System.out.println("Chucha madre que pereza");
         System.out.println("pene"); */
--- Taller03/obj.java
+++ Taller03/obj.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
 public class obj {
-    //Aqui presentamos los objetos para que el main pueda leerlos
+    //Metodos que preguntaran y calcularan
+    
+    public static void Usuario() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Programa para calcular la formula general");
+        System.out.println("Ingrese la variable A:");
+        int A = scanner.nextInt();
+        System.out.println("Ingrese la variable B:");
+        int B = scanner.nextInt();
+        System.out.println("Ingrese la variable C:");
+        int C = scanner.nextInt();
+    }
 }
