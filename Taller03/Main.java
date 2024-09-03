913697cf40b4ca79d97d03d973d66fbd40812cc7
Alexis Gonzalez <alexis.gonzalez13@utp.ac.pa>
2024-09-02 20:44:49 -0500

    Las 2 variables (trabajo honesto)

--- Taller03/Main.java
+++ Taller03/Main.java
@@ -1,7 +1,13 @@
 class Main {
     public static void main(String[] args) {
+        
+        //Declaracion de las variables que se vana usar
+        int a, b, c;
+        float x1, x2;
+        
 
-       System.out.println("Chucha madre que pereza");
-        System.out.println("pene");
+        /* Chistoso JIjijija (Porfavor borrar ahorita ta de motivacion)
+        System.out.println("Chucha madre que pereza");
+        System.out.println("pene"); */
     }
 }
