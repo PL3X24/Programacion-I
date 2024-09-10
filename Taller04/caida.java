def7b0520776403e796685c980c01dce256eb5a9
Alexis Gonzalez <alexis.gonzalez13@utp.ac.pa>
2024-09-09 23:08:55 -0500

    Terminacion de la clase caida y comentarios

--- Taller04/caida.java
+++ Taller04/caida.java
@@ -1,14 +1,32 @@
 package Taller04;
 
 public class caida {
-    
+    // Declarar variables para las operaciones
+    int altura = 40;
+    double gravedad = 9.81;
+    double respuesta;
+
     // Salida de presentacion
-    private void titulos(){
+    void titulos(){
         System.out.println(" Universidad tecnologica de panama");
 	    System.out.println(" Estudiante Oliver Batista 8-1028-1845");
         System.out.println(" Estudiante Alexis Gonzales 8-1026-1272");
     }
-    private void Calculos(){
-        
+
+    // Funcion para calculos
+    void Calculos(){
+        System.out.println(" La altura preseleccionada sera 40m");
+	    System.out.println(" La gravedad preseccionada sera 9.81m/s2");
+
+       respuesta = Math.sqrt(2 * altura / gravedad);
+    }
+
+    // Funcion que imprime los resultados
+    void Resultado(){
+        System.out.println("Altura del edificio = " + altura);
+        System.out.println("Gravedad de la tiera =  " + gravedad);
+        System.out.println("Tiempo de caida     =  " + respuesta);
+        System.out.println(" ");
     }
 }
+
--- Taller04/main.java
+++ Taller04/main.java
@@ -3,26 +3,51 @@ import java.util.Scanner;
 
 public class main {
 
-    public static void Main(String[] args){
+    public static void main(String[] args){
         
-        int i = 0;
+        // Scanner y deficinion de i para el bucle
         Scanner scanner = new Scanner(System.in);
-        int j = 0;
+        int i = 0;
+
+        // Creacion de los objetos
+        caida caida = new caida();
+
+        
+        caida.titulos();
+
+        // Menu interactivo
         while (i == 0) {
 
             System.out.println("Que desea hacer");
             System.out.println("[1] Calcular el tiempo de caida libre");
             System.out.println("[2] Calcular el monto final depositado en un banco");
-
+            System.out.println("[3] Salir del programa");
+            int j = scanner.nextInt();
         
-
-
+            // Logica del menu
             switch (j) {
+
+                // Caida libre
                 case 1:
                     
+                    caida.Calculos();
+                    caida.Resultado();
+                    System.out.println("");
                     break;
-            
+                
+                // Interes
+                case 2:
+
+                    break;
+
+                // Bye bye
+                case 3:
+                    System.out.println("Saliendo...");
+                    i = 1;
+                    break;
                 default:
+                System.out.println("Opcion no valida");
+                System.out.println("");
                     break;
             }
         }
